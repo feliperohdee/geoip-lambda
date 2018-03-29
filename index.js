a802f07681b1f3d9e1de818da2cf215e1b75999e
Felipe Rohde <feliperohdee@gmail.com>
2018-03-29 13:29:36 -0300

    simplify response

--- index.js
+++ index.js
@@ -3,58 +3,15 @@ const maxmind = require('maxmind');
 let lookup;
 
 const load = () => maxmind.openSync(path.resolve(`${__dirname}/GeoLite2-City.mmdb`));
-const formatToApi = (body, statusCode = 200) => {
-    return {
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        statusCode
-    }
-};
-
-const parseFromApi = event => {
-    let {
-        body = {},
-            headers = {},
-            pathParameters = {},
-            queryStringParameters = {}
-    } = event;
-
-    if (typeof body === 'string') {
-        body = JSON.parse(body);
-    }
-
-    return {
-        params: Object.assign({},
-            body,
-            pathParameters,
-            queryStringParameters
-        ),
-        headers
-    };
-};
 
 exports.handler = (event, context, callback) => {
     if (!lookup) {
         lookup = load();
     }
 
-    if (event.requestContext) {
-        const {
-            params,
-            headers
-        } = parseFromApi(event);
-
-        return callback(null, formatToApi(params.ip ? lookup.get(params.ip) : {}));
-    }
-
     callback(null, event.ip ? lookup.get(event.ip) : {});
 };
 
 // exports.handler({
-//     body: {
-//         ip: '138.186.164.84'
-//     },
-//     requestContext: true
+//     ip: '138.186.164.84'
 // }, null, console.log);
